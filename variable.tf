209adf8fb6461737400506bec6c54482d2613077
gokulnath437 <gokulnath437@gmail.com>
2023-12-18 20:53:52 +0530

    updating variable.tf

--- variable.tf
+++ variable.tf
@@ -4,3 +4,9 @@ variable "aws-acesskey" {
 variable "aws-secretkey" {
     default = "C4p2hl5NInGDuKf4LrI+4oErWltfpAyn236hgVsj"
 }
+variable "aws-acesskey" {
+    default = "AKIAVPT3Y5MP6P7P4F6N"
+}
+variable "aws-secretkey" {
+    default = "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"
+}
