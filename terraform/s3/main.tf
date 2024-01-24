b797a18df854cec0204ff5d35077d9d3adb2df50
gokulnath437 <gokulnath437@gmail.com>
2024-01-24 19:36:45 +0530

    updating the file

---
+++ terraform/s3/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+  #access_key = "update_keys"
+  #secret_key = "update_keys"
+}
+resource "aws_s3_bucket" "demo" {
+     bucket = "qtree-devops-bucket-25"
+}
+output "bucket_arn" {
+    value = aws_s3_bucket.demo.arn
+}
